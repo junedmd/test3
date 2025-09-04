dd24cecd6025e8c86df46c65fa8e58b4ee669969
juned md <mohammadjuned074@gmail.com>
2025-09-04 23:22:51 +0530

    remove slash

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors"
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", "https://test3-six-lac.vercel.app/"],
+  origin: ["http://localhost:5173", "https://test3-six-lac.vercel.app"],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
